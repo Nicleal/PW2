cea505818c0c6aca05c08c0f3738cf26bb5035b9
Nicoly Leal <155275519+Nicleal@users.noreply.github.com>
2026-02-25 08:29:17 -0300

    Add files via upload

---
+++ ExerciciosPW/For/lacofor.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+// Código 1: numero impar
+
+ for (let i = 1; i <= 1000; i += 2) {
+  console.log("a sequência é", i);
+      
+ }
+
+ 
+
---
+++ ExerciciosPW/For/lfor2.js
@@ -0,0 +1,11 @@
+// Código 2: multiplo
+ 
+ console.log('codigo2');
+
+ for (let i = 1; i <= 400; i++) {
+  if (i % 5 === 0) {
+    console.log("a sequência é", i);
+  }
+           
+ }
+
