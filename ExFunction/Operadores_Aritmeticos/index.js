53d7f4dc34325f141717bf46a91dbb79585375e4
Nicoly Leal <155275519+Nicleal@users.noreply.github.com>
2026-03-11 21:58:53 -0300

    Add files via upload

---
+++ ExFunction/Operadores_Aritmeticos/index.js
@@ -0,0 +1,25 @@
+import{soma,subtracao,multiplicacao,divisao,resto,potenciacao,raizQuadrada} from './operacoes.js';
+
+let a = 16;
+let b = 5;
+
+//soma 
+console.log(`Soma: ${soma(a, b)}`);
+
+//subtração
+console.log(`Subtração: ${subtracao(a, b)}`);
+
+//multiplicação
+console.log(`Multiplicação: ${multiplicacao(a, b)}`);
+
+//divisão
+console.log(`Divisão: ${divisao(a, b)}`);
+
+//resto
+console.log(`Resto: ${resto(a, b)}`);
+
+//potenciação
+console.log(`Potenciação: ${potenciacao(a, b)}`);
+
+//raiz quadrada
+console.log(`Raiz Quadrada de ${a}: ${raizQuadrada(a)}`);
---
+++ ExFunction/Operadores_Aritmeticos/operacoes.js
@@ -0,0 +1,34 @@
+//Soma
+export function soma(a, b) {
+  return a + b;
+}
+
+//Subtração
+export function subtracao(a, b) {
+  return a - b;
+}
+
+//Multiplicação
+export function multiplicacao(a, b) {
+  return a * b;
+}
+
+//Divisão
+export function divisao(a, b) {
+  return a / b;
+}
+
+//Resto
+export function resto(a, b) {
+  return a % b;
+}
+
+//Potenciação
+export function potenciacao(a, b) {
+  return a ** b;
+}
+
+//Raiz quadrada
+export function raizQuadrada(a) {
+  return Math.sqrt(a);
+}
