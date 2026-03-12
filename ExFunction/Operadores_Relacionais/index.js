ac0081cde64cbd2161dfa29994cca26e299af000
Nicoly Leal <155275519+Nicleal@users.noreply.github.com>
2026-03-11 22:00:22 -0300

    Add files via upload

---
+++ ExFunction/Operadores_Relacionais/index.js
@@ -0,0 +1,11 @@
+import{maiorQue,menorQue,maiorOuIgualA,menorOuIgualA,igualA,diferenteDe} from './opeRelacionais.js';
+
+let x = 5;
+let y = 10;
+
+console.log(maiorQue(x,y));
+console.log(menorQue(x,y));
+console.log(maiorOuIgualA(x,y));
+console.log(menorOuIgualA(x,y));
+console.log(igualA(x,y));
+console.log(diferenteDe(x,y));
---
+++ ExFunction/Operadores_Relacionais/opeRelacionais.js
@@ -0,0 +1,29 @@
+// Maior que
+export function maiorQue(x,y) {
+    return x > y;
+}
+
+// Menor que
+export function menorQue(x,y) {
+    return x < y;
+}
+
+// Maior ou igual a
+export function maiorOuIgualA(x,y) {
+    return x >= y;
+}
+
+// Menor ou igual a
+export function menorOuIgualA(x,y) {
+    return x <= y;
+}
+
+// Igual a
+export function igualA(x,y) {
+    return x === y;
+}
+
+//Diferente de
+export function diferenteDe(x,y) {
+    return x !== y;
+}
